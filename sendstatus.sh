4457e78d5b12b8bf7548b044c0d8c48dcc92cd33
gyogyika <info@gyogyi.info>
2022-06-17 20:35:06 +0200

    CPU_load

--- sendstatus.sh
+++ sendstatus.sh
@@ -5,6 +5,11 @@ source /root/settings
 CPU=$(top -b -n1 | awk '/^%Cpu/{print $2$3" "$4$5" "$6$7" "$8$9" "$10$11" "$12$13" "$14$15" "$16$17}')
 echo "CPU: $CPU"
 
+CPU_load=$(echo "$CPU" | awk '{print $4}')
+CPU_load=$(echo "$CPU_load" | awk -F, '{print $1}')
+CPU_load=$((100-CPU_load))
+echo "CPU_load: $CPU_load"
+
 CPU_temp=$(sensors | awk '/Package/ {print $4}')
 echo "CPU_temp: $CPU_temp"
 
